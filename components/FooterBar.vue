16ba212b7b58aa1f50cfaa1fc08441dfd7821c1e
Fumiya Sakai <just1factory@gmail.com>
2018-10-08 08:54:54 +0900

    modify templates

---
+++ components/FooterBar.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="hero-foot">
+    <div class="container">
+      <div class="tabs is-centered">
+        <ul>
+          <li>
+            <a href="#">
+              <i class="fab fa-facebook-square fa-2x" />
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <i class="fab fa-twitter-square fa-2x" />
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <i class="fab fa-twitter-square fa-2x" />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+ 
+<script>
+export default {
+  name: 'FooterBar'
+}
+</script>
---
+++ components/NavigationBar.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="hero-head">
+    <nav class="navbar">
+      <div class="container">
+        <div class="navbar-brand">
+          <a class="navbar-item" href="#">
+            <img src="https://bulma.io/images/bulma-logo.png" alt="Logo">
+          </a>
+          <span class="navbar-burger burger" data-target="navbarMenu">
+            <span />
+            <span />
+            <span />
+          </span>
+        </div>
+        <div id="navbarMenu" class="navbar-menu">
+          <div class="navbar-end">
+            <div class="tabs is-right">
+              <ul>
+                <li class="is-active"><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Archives</a></li>
+                <li><a href="#">Contact</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NavigationBar'
+}
+</script>
