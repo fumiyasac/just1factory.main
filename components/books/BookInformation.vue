7bba25b80edfcee9cc25bd5896e1c02ed2c46f1c
Fumiya Sakai <just1factory@gmail.com>
2020-09-12 22:11:12 +0900

    新たなラインナップの追加

--- components/books/BookInformation.vue
+++ components/books/BookInformation.vue
@@ -198,6 +198,62 @@
         </b-col>
       </b-row>
       <hr>
+      <!-- UI Recipe book meals2 -->
+      <b-row class="pt-4">
+        <b-col cols="3">
+          <div>
+            <b-img thumbnail fluid class="img-responsive" src="/books/ui_recipt_book_meals2.jpg" />
+          </div>
+        </b-col>
+        <b-col cols="9">
+          <div class="container">
+            <h4 class="developer_name">
+              iOSアプリ開発「UI実装であると嬉しいレシピブック おもしろ編」
+            </h4>
+            <p>
+              <span class="small">頒布イベント</span>:
+              &nbsp;
+              <b-badge pill variant="info">
+                技術書典9
+              </b-badge>
+            </p>
+          </div>
+          <div class="container pt-3">
+            <p>
+              <span class="small">販売価格（同人誌版）</span>: <strong class="large text-danger">¥1,000</strong><br>
+              <span class="small">※1 現在は電子版のみ販売中です。</span><br>
+              <span class="small">※2 収録サンプルはXcode11.6 &amp; Swift5.2に対応しております。</span>
+            </p>
+          </div>
+          <div class="container pt-3">
+            <p>これまでの書籍ではUI実装のアイデアや具体的な手法についてフォーカスを当てた書籍を3冊執筆してきましたが、急遽Vol.3の前段として更に番外編として表現や動きが特徴的でかつ、ユーザーにもほんの少しだけ遊び心を与えるような楽しい感覚を抱かせてくれるようなUI実装に関する解説を「おもしろ編（番外編Vol.2）」として簡単でありますがまとめたものになります。</p>
+            <p>Chapter1及びChapter2で紹介しているサンプル実装についてはUIKitをベースに構築したサンプルとなっていますが、Chapter3ではiOS13から登場したSwiftUIを利用して構築したサンプルとなっております。</p>
+            <p>これまでの実務の中で培ってきた知識や知見に加えて、一般的なiOSアプリに対しては利用可能なケースは限られてしまうかもしれませんが、アニメーションやインタラクションにひと工夫を加えることによって、見た目にも美しく触っていて思わず楽しくなりそうな感じのUI実装のエッセンスを盛り込んだサンプルを3点収録しております。</p>
+          </div>
+          <div class="container pt-3">
+            <p>
+              <span class="small">書籍内で解説しているサンプルコードにつきましては、Github上で無償で公開しております。サンプルコードで誤りや不明な点がある場合にはGithubのIssueやPullRequestをお送り頂けますと幸いです。</span>
+            </p>
+            <ul class="list-unstyled">
+              <li class="mt-4 mb-4">
+                <a href="https://github.com/fumiyasac/meals_2nd_ios_ui_recipe_showcase" target="_blank">
+                  <i class="fa fa-github github_color link_icon" />書籍掲載サンプルコード
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div class="container text-center pt-2">
+            <ul class="list-unstyled list-inline">
+              <li class="list-inline-item">
+                <b-button class="mt0" href="https://booth.pm/ja/items/2360379" target="_blank">
+                  書籍の内容を確認する
+                </b-button>
+              </li>
+            </ul>
+          </div>
+        </b-col>
+      </b-row>
+      <hr>
     </div>
   </div>
 </template>
